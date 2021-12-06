98eeffee91a5047bc482a206ede098d159ab5f3e
Lorenzo Costa <lorenzo@pop-os.localdomain>
2021-12-06 13:15:26 +0000

    request.py

--- request.py
+++ request.py
@@ -1,33 +1,65 @@
+from typing import Union
 import requests
 from pprint import pprint
 
-LOREN_IPSUM = """
-Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat
-Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-"""  
-HEADERS = {"Content-Type":"application/json"}
-BASE_URL = "http://164.90.191.234:5000/v1"
+from requests.models import Response
 
-def get_request(url):
-    r = requests.get(url=url,headers=HEADERS)
+
+HEADERS = {"Content-Type": "application/json"}
+BASE_URL = "http://161.35.68.36:5000/v1"
+LAST_EPISODE_ID = "61adffa2323daf7665ef5cbf"
+
+
+def get_request(url: str) -> Response:
+    r = requests.get(url=url, headers=HEADERS)
     return r.json()
 
 
-def post_request(url,json_data):
-    r = requests.post(url=url,headers=HEADERS,json=json_data)
+def post_request(url: str, json_data: dict) -> Response:
+    r = requests.post(url=url, headers=HEADERS, json=json_data)
     return r.json()
 
-ep_sample = "61a3d27009cac5993a0a5ac3"
 
-# edit_comment = post_request(f"{BASE_URL}/comments",{"newText":"ok, I guess...","id":"61a3d3c2041bbe209837455b"})
-# episode_info = get_request(f"{BASE_URL}/episodes?id={ep_sample}")
-# post_comment = post_request(f"{BASE_URL}/comments/episodes",{"comment":"really Bad","id":ep_sample})
-comments_by_episode = get_request(f"{BASE_URL}/comments/episodes?id={ep_sample}")
-# delete_comment = get_request(f"{BASE_URL}/comments/delete?id=61474fd32fd0ed2f706a4fd5")
-# episodes_rating = get_request(f"{BASE_URL}/episodes/ratings/above?rating=9.5&season=3")
-# episodes_rating_season = get_request(f"{BASE_URL}/episodes/ratings/above?rating=5&season=8")
+#-----API FUNCTIONS-------------------
 
-pprint(comments_by_episode)
+def get_comments_by_episode(episode_id: str):
+    comments_by_episode = get_request(
+        f"{BASE_URL}/comments/episodes?id={episode_id}")
+    pprint(comments_by_episode)
+
+
+def edit_comment(comment: str, comment_id: str):
+    edit_comment = post_request(
+        f"{BASE_URL}/comments", {"newText": comment, "id": comment_id})
+    pprint(edit_comment)
+
+
+def post_comment(comment: str, episode_id: str):
+    post_comment = post_request(
+        f"{BASE_URL}/comments/episodes", {"comment": comment, "id": episode_id})
+    pprint(post_comment)
+
+
+def get_episode_info(episode_id: str):
+    episode_info = get_request(f"{BASE_URL}/episodes?id={episode_id}")
+    pprint(episode_info)
+
+
+def delete_comment(comment_id: str):
+    delete_comment = get_request(f"{BASE_URL}/comments/delete?id={comment_id}")
+    pprint(delete_comment)
+
+
+def get_episodes_by_rating_above(rating: float, season: Union[int, None] = None):
+    if not season:
+        episodes_rating = get_request(
+            f"{BASE_URL}/episodes/ratings/above?rating={rating}")
+        pprint(episodes_rating)
+        return
+    episodes_rating = get_request(
+        f"{BASE_URL}/episodes/ratings/above?rating={rating}&season={season}")
+    pprint(episodes_rating)
+    return
+
+
+get_episodes_by_rating_above(95.8)
